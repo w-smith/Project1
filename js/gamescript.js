3e465139dbcbee7d70727af1dfeca297d93c6cf0
w-smith <wsmith@bex.net>
2017-12-14 15:01:56 -0700

    adding hit detection on y axis

--- js/gamescript.js
+++ js/gamescript.js
@@ -1,4 +1,9 @@
 console.log("Hey, it's me. Ya boii.. Kirby.");
+$(document).ready(function() {
+
+
+
+
 
 
 // KIRBY CONTROLS
@@ -6,13 +11,17 @@ $(window).keypress(function(q) {
     if(q.keyCode == 113)
     // console.log("racer1");
 $("#kirby").animate({marginTop: "-=3%"},-50);
+
 });
+
 
 $(window).keypress(function(a) {
     if(a.keyCode == 97)
     // console.log("racer1");
 $("#kirby").animate({marginTop: "+=3%"},-50);
 });
+
+
 
 
 
@@ -36,30 +45,117 @@ $("#metaknight").animate({marginTop: "+=3%"},-50);
 
 // // BIRDON1
 // $(document).ready(function() {
-//     $("#birdon1").animate({right: "+=2500"},10000);
+//     $("#birdon1").animate({right: "+=120%"},5000);
 //     // $("#birdon").animate({right: "-=300"}, 1000);
 // });
 
 // // BIRDON2
 // $(document).ready(function() {
-//     $("#birdon2").animate({right: "+=2500"},10000);
+//     $("#birdon2").animate({right: "+=120%"},5000);
 //     // $("#birdon").animate({right: "-=300"}, 1000);
 // });
 
 // // BIRDON3
 // $(document).ready(function() {
-//     $("#birdon3").animate({right: "+=2500"},10000);
+//     $("#birdon3").animate({right: "+=120%"},5000);
 //     // $("#birdon").animate({right: "-=300"}, 1000);
 // });
 
 
 
+// // BIRDON4
+// $(document).ready(function() {
+//     $("#birdon4").animate({right: "+=2500"},10000);
+//     // $("#birdon").animate({right: "-=300"}, 1000);
+// });
 
+// // BIRDON4
+// $(document).ready(function() {
+//     $("#birdon5").animate({right: "+=2500"},10000);
+//     // $("#birdon").animate({right: "-=300"}, 1000);
+// });
+
+// // BIRDON6
+
+//     $("#birdon6").animate({right: "+=2500"},10000);
+//     // $("#birdon").animate({right: "-=300"}, 1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var kirby = $("#kirby");
+var kwidth = 168;
+
+var skull1 = $("#skull1");
+var sk1width = 60;
+
+var metaknight = $("#metaknight");
+var mkwidth = 130;
+
+
+var kposition = kirby.position();
+$("#kirby").text(console.log(kposition.left, kposition));
+
+var mkposition = metaknight.position();
+$("#metaknight").text(console.log(mkposition.left, mkposition));
+
+var sk1position = skull1.position();
+$("#skull1").text(console.log(sk1position.left, sk1position));
+
+var metaknightright = mkposition.left + mkwidth;
+
+var kirbyright = kirby.position().left + kwidth;
+console.log(kirby.position().left);
+
+
+
+
+
+
+
+// if ( sk1position.left <= kirbyright ) {
+//   console.log("dead");
+// }
 
 // SKULL1
-$(document).ready(function() {
-    $("#skull1").animate({right: "+=120%"},12000);
-});
+ 
+
+$(skull1).animate({left: "-=20%"},1000, skullmove);
+function skullmove(){
+  // console.log(skull1.position().left);
+if ( skull1.position().left <= kirbyright ) {
+  console.log("dead");
+}
+else {
+$("#skull1").animate({left: "-=20%"},1000, skullmove);
+
+}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 // // SKULL2
 // $(document).ready(function() {
@@ -68,5 +164,9 @@ $(document).ready(function() {
 
 
 
+// TIMER
 
 
+
+
+});
